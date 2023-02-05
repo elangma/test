1064fe730e729e8fb4447a8cdc62881c09eeb85e
elangma <124343669+elangma@users.noreply.github.com>
2023-02-05 20:55:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-const config = require("platformsh-config").config();
-const port = config.port || 3000
-// const port = process.env.PORT || 3000;
+// const config = require("platformsh-config").config();
+// const port = config.port || 3000
+const port = process.env.PORT || 3000;
 
 var exec = require("child_process").exec;
 const os = require("os");
